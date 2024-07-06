24f258a558dd7a39b6b59d5b693f247c1e9642a9
davidliyutong <32391509+davidliyutong@users.noreply.github.com>
2024-07-06 23:37:50 +0800

    Fix header

--- src/client/src/ApiClient.js
+++ src/client/src/ApiClient.js
@@ -55,7 +55,7 @@ class ApiClient {
          * @default {}
          */
         this.defaultHeaders = {
-            // 'User-Agent' omitted: browsers refuse to set this header
+            // 'User-Agent': 'OpenAPI-Generator/1.0.0/Javascript'
         };
 
         /**
